758205eaf74431c56817426e44aca9b9b53cbd12
Pratul Bhatt <tggame9@gmail.com>
2024-09-11 20:49:22 +0530

    Added new Chnages

--- MahaDev.ps1
+++ MahaDev.ps1
@@ -546,3 +546,56 @@ Get-Content C:\windows10upgrade.log | Select-String "Error"
 # Regex - Regular Expression https://regexr.com/
 
 $time | Select-String -Pattern '(\d+\.\d+s)$'
+
+
+# ------------------------------------------------------------- x ----------------------------------------------------------
+
+<#
+11-09-2024
+----------
+#>
+
+    <#
+    .SYNOPSIS
+    Outputs "Hello, World!"
+    
+    .DESCRIPTION
+    Output "Hello, World!".
+    
+    .EXAMPLE
+    Get-HelloWorld
+    #>	
+
+
+
+    $n = 17
+    function CollatzConjecture {
+        param (
+            # To ensure $n should be atleast 1 and not -ve
+            [Parameter(Mandatory=$true)]
+            [ValidateRange(1, [Int64]::Maxvalue)]
+            [Int64] $n
+        )
+        
+        # Initialize step
+        $steps = 0
+
+        # Real function start
+
+        while ($n -ne 1) {
+            if ($n % 2 -eq 0) {
+                <# If $n is even divide by 2#>
+                $n = $n / 2
+            } else {
+                <# If $n is odd multiply by 3 and add 1 #>
+                $n = $n * 3 + 1
+            }
+            # Increment the steps count
+            $steps++
+        }
+
+        # Return steps
+        return $steps
+    }
+
+    CollatzConjecture
